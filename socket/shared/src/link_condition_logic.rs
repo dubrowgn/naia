9ef309d47d8063f8274fd646be67154166f53464
Dustin Brown <me@dubrowgn.com>
2025-07-01 08:38:26 -0600

    Add packet duplication to link conditioner

--- socket/shared/src/link_condition_logic.rs
+++ socket/shared/src/link_condition_logic.rs
@@ -5,20 +5,23 @@ use super::{link_conditioner_config::LinkConditionerConfig, time_queue::TimeQueu
 /// Given a config object which describes the network conditions to be
 /// simulated, process an incoming packet, adding it to a TimeQueue at the
 /// correct timestamp
-pub fn process_packet<T: Eq>(
-    config: &LinkConditionerConfig,
-    time_queue: &mut TimeQueue<T>,
-    packet: T,
+pub fn process_packet<T: Clone + Eq>(
+	config: &LinkConditionerConfig, time_queue: &mut TimeQueue<T>, packet: T,
 ) {
-    if rand::thread_rng().gen_range(0.0..=1.0) < config.incoming_loss {
-        // drop the packet
-        return;
-    }
+	let mut packets = 1;
+	if rand::thread_rng().gen_range(0.0..=1.0) < config.loss_frac {
+		packets -= 1;
+	}
+	if rand::thread_rng().gen_range(0.0..=1.0) < config.duplication_frac {
+		packets += 1;
+	}
 
-	let min = config.incoming_latency.saturating_sub(config.incoming_jitter);
-	let max = config.incoming_latency.saturating_add(config.incoming_jitter);
-	let latency_ms = rand::thread_rng().gen_range(min..=max);
-    let timestamp = Instant::now() + Duration::from_millis(latency_ms.into());
+	let min = f32::max(0.0, config.half_rtt_ms - config.jitter_ms);
+	let max = f32::min(config.half_rtt_ms + config.jitter_ms, f32::MAX);
 
-    time_queue.add_item(timestamp, packet);
+	for _ in 0..packets {
+		let half_rtt_ms = rand::thread_rng().gen_range(min..=max);
+		let timestamp = Instant::now() + Duration::from_secs_f32(half_rtt_ms / 1000.0);
+		time_queue.add_item(timestamp, packet.clone());
+	}
 }
--- socket/shared/src/link_conditioner_config.rs
+++ socket/shared/src/link_conditioner_config.rs
@@ -1,54 +1,35 @@
-/// Contains configuration required to initialize a LinkConditioner
 #[derive(Clone)]
 pub struct LinkConditionerConfig {
-    /// Delay to receive incoming messages in milliseconds
-    pub incoming_latency: u32,
-    /// The maximum additional random latency to delay received incoming
-    /// messages in milliseconds. This may be added OR subtracted from the
-    /// latency determined in the `incoming_latency` property above
-    pub incoming_jitter: u32,
-    /// The % chance that an incoming packet will be dropped.
-    /// Represented as a value between 0 and 1
-    pub incoming_loss: f32,
+	/// Base delay added to all received packets, in milliseconds
+	pub half_rtt_ms: f32,
+	/// Spread of the delay added to all received packets, in milliseconds.
+	/// Total delay is picked randomly from the range `half_rtt_ms` +/- `jitter_ms`.
+	pub jitter_ms: f32,
+	/// The fraction of incoming packets that will be dropped, between 0 and 1.
+	pub loss_frac: f32,
+	/// The fraction of incoming packets that will be duplicated, between 0 and 1.
+	pub duplication_frac: f32,
 }
 
 impl LinkConditionerConfig {
-    /// Creates a new LinkConditionerConfig
-    pub fn new(incoming_latency: u32, incoming_jitter: u32, incoming_loss: f32) -> Self {
-        LinkConditionerConfig {
-            incoming_latency,
-            incoming_jitter,
-            incoming_loss,
-        }
-    }
+	pub const GOOD: Self = Self::new(40.0, 6.0, 0.002, 0.002);
+	pub const AVERAGE: Self = Self::new(170.0, 45.0, 0.02, 0.02);
+	pub const POOR: Self = Self::new(300.0, 84.0, 0.04, 0.04);
 
-    /// Creates a new LinkConditioner that simulates a connection which is in a
-    /// good condition
-    pub fn good_condition() -> Self {
-        LinkConditionerConfig {
-            incoming_latency: 40,
-            incoming_jitter: 6,
-            incoming_loss: 0.002,
-        }
-    }
+	// some networks stat sources:
+	// * https://www.verizon.com/business/terms/latency
+	// * https://www.gin.ntt.net/support-center/service-level-agreements-slas/our-global-ip-network
+	// * https://radar.cloudflare.com/quality
+	pub const ASIA_EUROPE: Self = Self::new(142.5, 10.0, 0.003, 0.003);
+	pub const INTRA_USA: Self = Self::new(25.0, 7.0, 0.001, 0.001);
+	pub const SATELLITE: Self = Self::new(345.5, 34.55, 0.001, 0.001);
+	pub const TRANS_ATLANTIC: Self = Self::new(40.0, 10.0, 0.001, 0.001);
+	pub const TRANS_PACIFIC: Self = Self::new(65.0, 10.0, 0.001, 0.001);
+	pub const WIFI_GOOD: Self = Self::new(3.1, 3.756, 0.005, 0.005);
 
-    /// Creates a new LinkConditioner that simulates a connection which is in an
-    /// average condition
-    pub fn average_condition() -> Self {
-        LinkConditionerConfig {
-            incoming_latency: 170,
-            incoming_jitter: 45,
-            incoming_loss: 0.02,
-        }
-    }
-
-    /// Creates a new LinkConditioner that simulates a connection which is in an
-    /// poor condition
-    pub fn poor_condition() -> Self {
-        LinkConditionerConfig {
-            incoming_latency: 300,
-            incoming_jitter: 84,
-            incoming_loss: 0.04,
-        }
-    }
+	pub const fn new(
+		half_rtt_ms: f32, jitter_ms: f32, loss_frac: f32, duplication_frac: f32
+	) -> Self {
+		LinkConditionerConfig { half_rtt_ms, jitter_ms, loss_frac, duplication_frac }
+	}
 }
