063c4052f1419fb5a66ebc58ba3a562f028c0d1c
Dustin Brown <me@dubrowgn.com>
2025-07-08 10:44:02 -0600

    Remove url_parse

--- socket/shared/src/lib.rs
+++ socket/shared/src/lib.rs
@@ -11,11 +11,9 @@
 
 mod link_conditioner_config;
 mod time_queue;
-mod url_parse;
 
 pub use link_conditioner_config::LinkConditionerConfig;
 pub use time_queue::TimeQueue;
-pub use url_parse::{parse_server_url, url_to_socket_addr};
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct ChannelClosedError<T>(pub T);
