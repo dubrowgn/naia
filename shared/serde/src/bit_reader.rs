a43401c42cc7afde9641e333ecca7cdb31480a99
Dustin Brown <me@dubrowgn.com>
2025-07-31 11:35:39 -0600

    Remove BitReaderState

--- shared/serde/src/bit_reader.rs
+++ shared/serde/src/bit_reader.rs
@@ -1,19 +1,19 @@
 use crate::SerdeErr;
 
 pub struct BitReader {
-    state: BitReaderState,
     buffer: Box<[u8]>,
+	buffer_index: usize,
+	scratch: u8,
+	scratch_index: u8,
 }
 
 impl BitReader {
     pub fn new(buffer: Box<[u8]>) -> Self {
         Self {
-            state: BitReaderState {
-                scratch: 0,
-                scratch_index: 0,
-                buffer_index: 0,
-            },
             buffer,
+			buffer_index: 0,
+			scratch: 0,
+			scratch_index: 0,
         }
     }
 
@@ -22,45 +22,34 @@ impl BitReader {
     }
 
     pub fn read_bit(&mut self) -> Result<bool, SerdeErr> {
-        if self.state.scratch_index == 0 {
-            if self.state.buffer_index == self.buffer.len() {
+        if self.scratch_index == 0 {
+            if self.buffer_index == self.buffer.len() {
                 return Err(SerdeErr);
             }
 
-            self.state.scratch = self.buffer[self.state.buffer_index];
+            self.scratch = self.buffer[self.buffer_index];
 
-            self.state.buffer_index += 1;
-            self.state.scratch_index += 8;
+            self.buffer_index += 1;
+            self.scratch_index += 8;
         }
 
-        let value = self.state.scratch & 1;
+        let value = self.scratch & 1;
 
-        self.state.scratch >>= 1;
+        self.scratch >>= 1;
 
-        self.state.scratch_index -= 1;
+        self.scratch_index -= 1;
 
         Ok(value != 0)
     }
 
     pub(crate) fn read_byte(&mut self) -> Result<u8, SerdeErr> {
         let mut output = 0;
-        for _ in 0..7 {
+        for _ in 0..8 {
+            output >>= 1;
             if self.read_bit()? {
                 output |= 128;
             }
-            output >>= 1;
-        }
-        if self.read_bit()? {
-            output |= 128;
         }
         Ok(output)
     }
-}
-
-// BitReaderState
-#[derive(Copy, Clone)]
-struct BitReaderState {
-    scratch: u8,
-    scratch_index: u8,
-    buffer_index: usize,
 }
